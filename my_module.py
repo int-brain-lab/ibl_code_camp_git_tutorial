f85d1d37cbf45d20ff86151e5d5bcd14dc90c787
Jean-Paul Noel <jeanpaulnc@gmail.com>
2019-09-02 17:59:04 -0400

    gitflow-feature-stash: Chris
    Again chris

--- my_module.py
+++ my_module.py
@@ -1,3 +1,5 @@
 print('Hello World!')
 print("Here's something interesting:\n")
 # Add lines here, let's make this script do something ineterting!! 
+print("hey this is Chris")
+print("Chris again")
